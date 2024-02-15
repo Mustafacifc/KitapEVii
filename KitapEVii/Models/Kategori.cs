94ca4fb194c0b2d893d6a76d1f7312cfc33f202e
mustafa471 <mustafa471@BPC12>
2024-02-15 09:15:28 +0300

    Proje dosyası ekle.

---
+++ KitapEVii/Models/Kategori.cs
@@ -0,0 +1,9 @@
+﻿namespace KitapEVii.Models
+{
+    public class Kategori
+    {
+        public int KategoriId { get; set; }
+        public string KategoriAdi { get; set; }
+        List<KitapModel> KitapListesi = new List<KitapModel>();
+    }
+}
